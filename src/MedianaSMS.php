8db78fe2e0f3f974a4d2802f32e5190172a77c8d
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2022-03-16 12:24:08 +0330

    Add get credit to mediana

--- src/MedianaSMS.php
+++ src/MedianaSMS.php
@@ -14,7 +14,7 @@ class MedianaSMS implements Gateway
     private string $pass;
     private string $from;
 
-    public function __construct( string $uname, string $pass, string $from,string $url = 'https://ippanel.com/services.jspd')
+    public function __construct(string $uname, string $pass, string $from, string $url = 'https://ippanel.com/services.jspd')
     {
         $this->url = $url;
         $this->uname = $uname;
@@ -23,8 +23,9 @@ class MedianaSMS implements Gateway
     }
 
 
-    
-    public function sendSMS(string $to,string $message):array{
+
+    public function sendSMS(string $to, string $message): array
+    {
         $param = [
             'uname' => $this->uname,
             'pass' => $this->pass,
@@ -45,34 +46,44 @@ class MedianaSMS implements Gateway
         $res_data = $response2[1];
 
 
-       
-        return [$res_code,$res_data];
-    }
-    public function sendOneSMSToMultiNumber(array $to,string $message){
 
+        return [$res_code, $res_data];
     }
-    public function sendMultiSMSToMultiNumber(array $msNum){
-
+    public function sendOneSMSToMultiNumber(array $to, string $message)
+    {
     }
-    public function ReciveSMS(){
-
+    public function sendMultiSMSToMultiNumber(array $msNum)
+    {
     }
-    public function getSMSStatus($messageId):string{
+    public function ReciveSMS()
+    {
+    }
+    public function getSMSStatus($messageId): string
+    {
         return "";
     }
-    public function getCredit():int{
-        return 0;
+    public function getCredit(): int
+    {
+        $param = array(
+            'uname' => $this->uname,
+            'pass' => $this->pass,
+            'op' => 'credit'
+        );
+
+        $handler = curl_init($this->url);
+        curl_setopt($handler, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($handler, CURLOPT_POSTFIELDS, $param);
+        curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
+        $response2 = curl_exec($handler);
+
+        $response2 = json_decode($response2);
+        $res_code = $response2[0];
+        $res_data = $response2[1];
+
+
+        return $res_data;
     }
-    public function addContact(array $contactInfo){
-
+    public function addContact(array $contactInfo)
+    {
     }
-
-
-
-
-
-
-
-
- 
 }
