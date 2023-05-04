6267867809668989289cea90697480d6397e633d
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2023-05-04 17:21:16 +0330

    Add send by pattern to farapayamak

--- src/FaraPayamak.php
+++ src/FaraPayamak.php
@@ -58,6 +58,31 @@ class FaraPayamak implements Gateway
         curl_close($curl);
         return (array)json_decode($response);
     }
+    public function sendSMSByPattern(string $to, string $message,int $bodyId): array
+    {
+       
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $this->url . 'BaseServiceNumber',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => 'username=' . $this->uname . '&password=' . $this->pass . '&to=' . $to . '&text=' . $message . '&bodyId=' . $bodyId,
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/x-www-form-urlencoded'
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        return (array)json_decode($response);
+    }
     public function sendOneSMSToMultiNumber(array $to, string $message)
     {
     }
